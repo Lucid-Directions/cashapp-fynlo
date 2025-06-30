c223b7380266c6cfc89970dd15a38e7758be0878
Ryan Davidson <ryand2626@gmail.com>
2025-06-30 23:09:28 +0100

    stage-2: ignore testSetup suite with no tests

--- CashApp-iOS/CashAppPOS/jest.unit.config.js
+++ CashApp-iOS/CashAppPOS/jest.unit.config.js
@@ -22,6 +22,7 @@ module.exports = {
     '<rootDir>/src/__tests__/integration',
     '<rootDir>/src/__tests__/utils',
     '<rootDir>/__tests__/utils',
+    '<rootDir>/__tests__/testSetup.ts',
   ],
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|react-native-vector-icons|react-native-gesture-handler|react-native-reanimated|@react-navigation|react-native-screens|react-native-safe-area-context|react-native-modal|sumup-react-native-alpha)/)',
